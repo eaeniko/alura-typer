756287f3618be596179e1652ef38c5c830dbb07b
Nikolai Cinotti <agencianiko@hotmail.com>
2017-01-17 20:35:39 -0200

    Contando as palavras e caracteres.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 
-
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
-
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+	var conteudo = campo.val();
+	//console.log(conteudo);
+	var qtdPalavras = conteudo.split(/\S+/).length -1;
+	$("#contador-palavras").text(qtdPalavras);
+
+	var qtdCaracteres = conteudo.length;
+	$("#contador-caracteres").text(qtdCaracteres);
+
+});
