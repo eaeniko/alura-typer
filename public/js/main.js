b25660d6beb78f35cafb2a3aae8fb04a5c3816be
Nikolai Cinotti <agencianiko@hotmail.com>
2017-01-21 00:33:09 -0200

    Contando tempo e utilizando .one

--- public/js/main.js
+++ public/js/main.js
@@ -15,3 +15,17 @@ campo.on("input", function(){
 	$("#contador-caracteres").text(qtdCaracteres);
 
 });
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function(){
+	var cronometroID = setInterval(function(){
+		tempoRestante--;
+		console.log(tempoRestante);
+		$("#tempo-digitacao").text(tempoRestante);
+		if(tempoRestante < 1) {
+			campo.attr("disabled", true);
+			clearInterval(cronometroID);
+		}
+	},1000);
+
+});
